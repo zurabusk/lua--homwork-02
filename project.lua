4110e7e431ec7f1bc0d7e0c229315bc68dd9eea1
zurabusk <buskivadzezura@gmail.com>
2023-06-06 13:46:46 +0400

    first commit

--- project.lua
+++ project.lua
@@ -1,6 +1,23 @@
-for number = 7, 1000, 7 do
-print(number)
+function calculateSum(first, last)
+	sum = 0
+	for number = first, last do
+		--if (number % 2 == 0) or (number % 9 == 0) then
+		--	sum = sum + number
+		--end
+		
+		if not (number % 2 == 0) and not (number % 9 == 0) then
+			goto continue
+		end
+		
+		
+		sum = sum + number
+		
+		::continue::
+	end
+	
+	return sum
 end
 
-
+sum = calculateSum(1, 10)
+print(sum)
 
